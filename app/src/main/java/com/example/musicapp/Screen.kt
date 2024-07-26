87c09215bf992dabb987a69467639da9f0fadd50
ChandrashekharChinta <chandrashekharchinta26@gmail.com>
2024-07-26 21:19:45 +0530

    Second Comment

---
+++ app/src/main/java/com/example/musicapp/Screen.kt
@@ -0,0 +1,36 @@
+package com.example.musicapp
+
+
+
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(val title: String, val route: String) {
+
+    sealed class  DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes val icon: Int)
+        : Screen(dTitle, dRoute){
+        object Account: DrawerScreen(
+            "Account",
+            "account",
+            R.drawable.ic_account
+        )
+        object Subscription: DrawerScreen(
+            "Subscription",
+            "subscribe",
+            R.drawable.ic_subscribe
+        )
+
+        object AddAccount: DrawerScreen(
+            "Add Account",
+            "add_account",
+            R.drawable.baseline_add_24
+        )
+    }
+
+}
+
+val screensInDrawer = listOf(
+    Screen.DrawerScreen.Account,
+    Screen.DrawerScreen.Subscription,
+    Screen.DrawerScreen.AddAccount
+)
